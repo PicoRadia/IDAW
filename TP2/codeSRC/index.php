17753a08b134e89ddd2935af225bc6d5b2f858c9
Radia EL HAMDOUNI <53488041+PicoRadia@users.noreply.github.com>
2021-03-10 11:58:22 +0100

    Refctored code with php

---
+++ TP2/codeSRC/index.php
@@ -0,0 +1,160 @@
+<?php include('header.php'); ?>
+<?php include('nav.php'); ?>
+
+  <div id="headerwrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+          <h2 style ="color :white;" >Hey , I'm Fast Dev your</h2>
+          <h1>Data Expert</h1>
+          <h3 style ="color :white;" >In Data scraping ,Data analysis , Machine learning , Deep Learning…… </h3>
+        </div>
+      </div>
+    </div>
+    <!-- /container -->
+  </div>
+
+  <section id="works"></section>
+  <div class="container">
+    <div class="row centered mt mb">
+      <h1>My projects</h1>
+
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>Automatic License Plate Detection</p>
+        <a href="https://youtu.be/d9EvrkpzyDA"><img src="img/portfolio/folio02.png" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>Chess Engine in C language - Demo</p>
+        <a href="https://youtu.be/7Jvm8QqaBI4"><img src="img/portfolio/folio01.png" class="img-responsive"></a>
+      </div>
+      
+      <br>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>Scraping hashtag count off Instagram
+          </p>
+        <a href="https://youtu.be/enAL-oS1jDU"><img src="img/portfolio/folio06.png" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>Data visualization with Dash</p>
+        <a href="https://youtu.be/G7mW8DVg-48"><img src="img/portfolio/folio04.png" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>web scraping the Toyota Simulator website</p>
+        <a href="https://youtu.be/chvmFWsF8Sg"><img src="img/portfolio/folio05.png" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <p>One liner for web scraping</p>
+        <a href="https://youtu.be/_hKSqEI7H48"><img src="img/portfolio/folio03.png" class="img-responsive"></a>
+      </div>
+      
+
+
+    </div>
+  </div>
+  <div class= 'container'>
+    <h1 class='text-center'>My skills</h1>
+        <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Web Developpement</h4><br>
+            <label>HTML -CSS - JavaScript - JQuery - Ajax</label>
+            <progress max="100" value="70"> 67% </progress>
+            <br>
+            <label>Flask / Django</label>
+            <progress id="file" max="100" value="70"> 60% </progress>
+            <br>
+            <label>PHP</label>
+            <progress max="100" value="70"> 50% </progress>
+            <br>
+            <label>Wordpress</label>
+            <progress max="100" value="70"> 60% </progress>
+        </div><br>
+        <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Web Scraping</h4><br>
+            <label>BeautifulSoup</label>
+            <progress value="70"> 90% </progress>
+            <br>
+            <label> Selenium </label>
+            <progress max="100" value="70"> 80% </progress>
+            <br>
+            <label> Scrapy </label>
+            <progress max="100" value="70"> 75% </progress>
+            <br>
+            <label>Cheerios JS</label>
+            <progress max="100" value="70"> 45% </progress>
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Machine learning</h4><br>
+            <label>Numpy - Pandas - matplotlib</label>
+            <progress value="70"> 67% </progress>
+            <br>
+            <label> scikit-learn </label>
+            <progress max="100" value="70"> 60% </progress>
+            <br>
+            <label> keras - TenserFlow</label>
+            <progress max="100" value="70"> 50% </progress>
+            <br>
+            <label>pytorch - fastAi</label>
+            <progress max="100" value="70"> 45% </progress>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Programming languages</h4><br>
+            <label>C - C++</label>
+            <progress value="70"> 90% </progress>
+            <br>
+            <label> Python</label>
+            <progress max="100" value="70"> 80% </progress>
+            <br>
+            <label>Java</label>
+            <progress max="100" value="70"> 75% </progress>
+            <br>
+            <label>Go  </label>
+            <progress max="100" value="70"> 45% </progress>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Soft Skills</h4><br>
+            <label>Creativity</label>
+            <progress value="70"> 78% </progress>
+            <br>
+            <label> Effective communication</label>
+            <progress max="100" value="70"> 72% </progress>
+            <br>
+            <label>Critical thinking</label>
+            <progress max="100" value="70"> 82% </progress>
+            <br>
+            <label>Adaptability</label>
+            <progress max="100" value="70"> 77% </progress>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-2">
+            <h4>Spoken languages</h4><br>
+            <label>Arabic</label>
+            <progress value="70"> 90% </progress>
+            <br>
+            <label> French</label>
+            <progress max="100" value="70"> 79% </progress>
+            <br>
+            <label>English</label>
+            <progress max="100" value="70"> 82% </progress>
+            <br>
+            <label>German</label>
+            <progress max="100" value="70"> 48% </progress>
+            <br>
+           
+      </div>
+    </ul>
+  </div>
+    <hr><br>
+<?php include('footer.php'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
